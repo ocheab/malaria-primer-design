bc852d87f338003e10090222dba4f97112acf529
ocheab <49044910+ocheab@users.noreply.github.com>
2025-05-28 23:27:40 +0100

    run_app.R

--- R/run_app.R
+++ R/run_app.R
@@ -1,13 +1,9 @@
-#' @import shiny
-#' @import shinyWidgets
-#' @import bslib
-#' @import Biostrings
-#' @import DT
-#' @import ggplot2
-#' @import seqinr
-#' @import stringr
-NULL
-# In R/run_app.R
+#' Launch the Malaria Primer Designer Shiny App
+#'
+#' This function launches the interactive user interface for designing PCR primers
+#' for malaria diagnostic markers across Plasmodium species.
+#'
+#' @export
 run_app <- function() {
   shiny::shinyApp(ui = ui, server = server)
 }
