7670e8651d8f584a15e3da867a9a86fef53acd55
ocheab <49044910+ocheab@users.noreply.github.com>
2025-05-28 22:30:42 +0100

    run_app.R

--- R/run_app.R
+++ R/run_app.R
@@ -1,3 +1,12 @@
+#' @import shiny
+#' @import shinyWidgets
+#' @import bslib
+#' @import Biostrings
+#' @import DT
+#' @import ggplot2
+#' @import seqinr
+#' @import stringr
+NULL
 # In R/run_app.R
 run_app <- function() {
   shiny::shinyApp(ui = ui, server = server)
